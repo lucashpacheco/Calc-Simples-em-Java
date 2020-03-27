a16105cc52bd615edbb5fd628b6573630220d98c
lucashpmendonca <59491850+lucashpmendonca@users.noreply.github.com>
2020-03-27 20:51:41 -0300

    Add files via upload
    calc simples trab iterface humana

---
+++ Calculadora Faculdade/Calculadora Faculdade/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Calculadora_Faculdade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtoperacao = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtresultado = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtaguardo = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(204, 204);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(204, 259);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 203);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // txtoperacao
+            // 
+            this.txtoperacao.AutoSize = true;
+            this.txtoperacao.Location = new System.Drawing.Point(91, 154);
+            this.txtoperacao.Name = "txtoperacao";
+            this.txtoperacao.Size = new System.Drawing.Size(13, 13);
+            this.txtoperacao.TabIndex = 6;
+            this.txtoperacao.Text = "?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 246);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "=";
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.AutoSize = true;
+            this.txtresultado.Location = new System.Drawing.Point(73, 275);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(55, 13);
+            this.txtresultado.TabIndex = 8;
+            this.txtresultado.Text = "Resultado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Lucas Pacheco";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Insira o primeiro Nº";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Insira o segundo Nº";
+            // 
+            // txtaguardo
+            // 
+            this.txtaguardo.AutoSize = true;
+            this.txtaguardo.Location = new System.Drawing.Point(40, 52);
+            this.txtaguardo.Name = "txtaguardo";
+            this.txtaguardo.Size = new System.Drawing.Size(122, 13);
+            this.txtaguardo.TabIndex = 12;
+            this.txtaguardo.Text = "Aguardando operação...";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(204, 47);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "RESET";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 325);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.txtaguardo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtoperacao);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label txtoperacao;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label txtresultado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label txtaguardo;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Calculadora Faculdade/Calculadora Faculdade/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_Faculdade
+{
+    public partial class Form1 : Form
+
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            subtrai();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            soma();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            multiplica();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dividi();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        public void soma()
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+
+            
+            txtoperacao.Text = Convert.ToString('+');
+
+            double resultado = a + b;
+
+            txtresultado.Text = Convert.ToString(resultado);
+
+            txtaguardo.Text = Convert.ToString("Operação Concluida");
+        }
+        public void subtrai()
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+
+            txtoperacao.Text = Convert.ToString('-');
+
+            double resultado = a - b;
+            
+            txtresultado.Text = Convert.ToString(resultado);
+
+            txtaguardo.Text = "Operação Concluida";
+
+
+
+        }
+
+        public void multiplica()
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            txtoperacao.Text = Convert.ToString('*');
+            double resultado = a * b;
+            txtresultado.Text = Convert.ToString(resultado);
+
+            txtaguardo.Text = "Operação Concluida";
+        }
+        public void dividi()
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            txtoperacao.Text = Convert.ToString('/');
+            double resultado = a - b;
+            txtresultado.Text = Convert.ToString(resultado);
+
+            txtaguardo.Text = "Operação Concluida";
+        }
+
+        public void reset()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            txtoperacao.Text = Convert.ToString('?');
+
+            txtresultado.Text = "Resultado";
+
+            txtaguardo.Text = "Aguardando operação...";
+        }
+
+        
+    }
+}
